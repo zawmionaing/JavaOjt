54bc34cdb0b517f994095a6de471a58012483f4d
zawmyoniang <zawmyonaing@ucstgi.edu.mm>
2023-08-16 08:54:22 +0630

    push to main

--- src/main/java/com/example/testing/TestingApplication.java
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
